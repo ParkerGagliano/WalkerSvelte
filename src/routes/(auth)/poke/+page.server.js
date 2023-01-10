c2faa85579b2ab6d0a796d6c4ebcc7eb5563cf31
parker gagliano <pjg4086@uncw.edu>
2023-01-09 21:17:41 -0500

    error handle

--- src/routes/(auth)/poke/+page.server.js
+++ src/routes/(auth)/poke/+page.server.js
@@ -1,11 +1,32 @@
 import { SECRET_API_KEY } from "$env/static/private";
-/** @type {import('./$types').PageServerLoad} */
-export async function load() {
-  let a = await fetch(
-    `https://maps.googleapis.com/maps/api/distancematrix/json?origins=1119+swordfish+lane,carolina+beach,NC&destinations=34.027134,-77.894682&mode=walking&key=${SECRET_API_KEY}
-    `,
-    { headers: { mode: "no-cors" } }
-  );
-  let data = await a.json();
-  return data;
-}
+
+import { db } from "$lib/database";
+/** @type {import('./$types').Actions} */
+export const actions = {
+  default: async (event) => {
+    let fd = await event.request.formData();
+    let address = fd.get("address");
+    console.log(address);
+    let final = address.split(" ");
+    console.log(final);
+    let addy = "";
+    final.forEach((element) => {
+      if (element == final[final.length - 1]) {
+        addy += element;
+      } else {
+        addy += element + "+";
+      }
+    });
+    console.log(addy);
+    let a = await fetch(
+      `https://maps.googleapis.com/maps/api/distancematrix/json?origins=${addy},carolina+beach,NC&destinations=34.027134,-77.894682&mode=walking&key=${SECRET_API_KEY}
+      `,
+      { headers: { mode: "no-cors" } }
+    );
+    let data = await a.json();
+    if (data.origin_addresses == "Carolina Beach, NC 28428, USA") {
+      return { error: "Address not found" };
+    }
+    return data;
+  },
+};
--- src/routes/(auth)/poke/+page.svelte
+++ src/routes/(auth)/poke/+page.svelte
@@ -1,10 +1,22 @@
 <script>
   import { onMount } from "svelte";
   import { fly } from "svelte/transition";
-  /** @type {import('./$types').PageServerData} */
-  export let data;
+
   let dataready = false;
-  console.log(data.rows[0].elements);
+  /** @type {import('./$types').ActionData} */
+  export let form;
+  console.log(form);
 </script>
 
-<h1>{data.rows[0].elements[0].duration.text}</h1>
+<form method="POST">
+  <input type="text" name="address" />
+  <button type="submit">Calculate</button>
+</form>
+
+{#if form?.rows}
+  <h1>{form?.rows[0].elements[0].duration.text} walk</h1>
+{/if}
+
+{#if form?.error}
+  <h1>{form?.error}</h1>
+{/if}
